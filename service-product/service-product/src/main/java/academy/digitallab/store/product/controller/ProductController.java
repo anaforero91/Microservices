ce2f3f532d9411308d55e1347d19fe2d4e631f12
Ana <anaforero91@gmail.com>
2025-03-11 13:23:39 -0500

    Finishing REST

--- service-product/service-product/src/main/java/academy/digitallab/store/product/controller/ProductController.java
+++ service-product/service-product/src/main/java/academy/digitallab/store/product/controller/ProductController.java
@@ -3,14 +3,22 @@ package academy.digitallab.store.product.controller;
 import academy.digitallab.store.product.entity.Category;
 import academy.digitallab.store.product.entity.Product;
 import academy.digitallab.store.product.service.ProductService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/products")
@@ -46,7 +54,10 @@ public class ProductController {
     }
 
     @PostMapping
-    public ResponseEntity<Product> createProduct(@RequestBody Product product){
+    public ResponseEntity<Product> createProduct(@Valid @RequestBody Product product, BindingResult result){
+        if (result.hasErrors()){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, this.formatMessage(result));
+        }
         Product productCreate = productService.createProduct(product);
         return ResponseEntity.status(HttpStatus.CREATED).body(productCreate);
     }
@@ -78,4 +89,25 @@ public class ProductController {
         }
         return ResponseEntity.ok(product);
     }
+
+    private String formatMessage(BindingResult result){
+        List<Map<String, String>> errors = result.getFieldErrors().stream()
+                .map(err ->{
+                    Map<String,String> error = new HashMap<>();
+                    error.put(err.getField(), err.getDefaultMessage());
+                    return error;
+                }).collect(Collectors.toList());
+        ErrorMessage errorMessage = ErrorMessage.builder()
+                .code("01")
+                .messages(errors).build();
+        ObjectMapper mapper = new ObjectMapper();
+        String jsonString = "";
+        try {
+            jsonString = mapper.writeValueAsString(errorMessage);
+        } catch (JsonProcessingException e){
+            e.printStackTrace();
+        }
+        return jsonString;
+    }
+
 }
--- service-product/service-product/src/main/java/academy/digitallab/store/product/entity/Product.java
+++ service-product/service-product/src/main/java/academy/digitallab/store/product/entity/Product.java
@@ -2,6 +2,9 @@ package academy.digitallab.store.product.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -20,8 +23,11 @@ public class Product {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private  Long id;
 
+    @NotEmpty(message = "El nombre no debe ser vacío")
     private  String name;
     private String description;
+
+    @Positive(message = "El stock debe ser mayor que cero")
     private Double stock;
     private Double price;
     private String status;
@@ -30,6 +36,7 @@ public class Product {
     @Temporal(TemporalType.TIMESTAMP)
     private Date createAt;
 
+    @NotNull(message = "La categoría no debe ser vacía")
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "category_id")
     @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
